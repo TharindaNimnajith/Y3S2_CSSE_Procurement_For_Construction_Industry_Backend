81939bda2b7de3c5b297992b200129c049206140
harshani2427 <harshaninimanthika2427@gmail.com>
2020-10-14 04:38:12 +0530

    added method for updating when created invoice

--- routes/order.routes.js
+++ routes/order.routes.js
@@ -8,5 +8,6 @@ router.get('/getOrders', OrderController.getOrders)
 router.get('/getOrders/:id', OrderController.getOrder)
 router.put('/editOrders', OrderController.editOrders)
 router.delete('/deleteOrders', OrderController.deleteOrders)
+router.post('/addInvoiceOrder/:id', OrderController.addInvoiceOrder)
 
 module.exports = router
