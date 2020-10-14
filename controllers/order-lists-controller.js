cb73e4718a1643c7867f5d23573765884a6663ab
TharindaNimnajith <tharindarajapakshe@gmail.com>
2020-10-14 14:24:24 +0530

    code refactoring

--- controllers/order-lists-controller.js
+++ controllers/order-lists-controller.js
@@ -2,259 +2,172 @@ const HttpError = require('../models/http-errors');
 const Orders = require('../models/order.model');
 
 const getPendingOrders = async (req, res, next) => {
-
-
   let orderList;
 
   try {
     orderList = await Orders.find({
       status: 'pending'
-
     });
-
-    console.log(orderList);
-
-
   } catch (error) {
     console.log(error);
     return next(new HttpError('Unexpected internal server error occurred, please try again later.', 500));
   }
+
   res.status(200).send(orderList);
-
-
 };
 
-
 const getApprovedPurchasedOrders = async (req, res, next) => {
-
-
   let orderList;
 
-
   try {
-    orderList = await Orders.find(
-      {
-        status: { $in: ['approved', 'pApproved'] }
-      });
-
-    console.log(orderList);
-
-
+    orderList = await Orders.find({
+      status: {
+        $in: ['approved', 'pApproved']
+      }
+    });
   } catch (error) {
     console.log(error);
     return next(new HttpError('Unexpected internal server error occurred, please try again later.', 500));
   }
+
   res.status(200).send(orderList);
-
-
 };
 
 const getRejectedPurchasedOrders = async (req, res, next) => {
-
-
   let orderList;
 
   try {
     orderList = await Orders.find({
       status: 'pRejected'
-
     });
-
-    console.log(orderList);
-
-
   } catch (error) {
     console.log(error);
     return next(new HttpError('Unexpected internal server error occurred, please try again later.', 500));
   }
+
   res.status(200).send(orderList);
-
-
 };
-
 
 const getPendingOrdersSupplier = async (req, res, next) => {
-
-
   let orderList;
 
   const {
     supplierName
   } = req.body;
 
-
   try {
-    orderList = await Orders.find(
-      {
-        status: { $in: ['approved', 'pApproved'] },
-        supplierName: supplierName
-      });
-
-    console.log(orderList);
-
-
+    orderList = await Orders.find({
+      status: {
+        $in: ['approved', 'pApproved']
+      },
+      supplierName: supplierName
+    });
   } catch (error) {
     console.log(error);
     return next(new HttpError('Unexpected internal server error occurred, please try again later.', 500));
   }
+
   res.status(200).send(orderList);
-
-
 };
-
 
 const getDeliveredOrdersSupplier = async (req, res, next) => {
-
-
   let orderList;
 
   const {
     supplierName
   } = req.body;
 
-
   try {
-    orderList = await Orders.find(
-      {
-        status: 'supDelivered',
-        supplierName: supplierName
-      });
-
-    console.log(orderList);
-
-
+    orderList = await Orders.find({
+      status: 'supDelivered',
+      supplierName: supplierName
+    });
   } catch (error) {
     console.log(error);
     return next(new HttpError('Unexpected internal server error occurred, please try again later.', 500));
   }
+
   res.status(200).send(orderList);
-
-
 };
-
 
 const getRejectedOrdersSupplier = async (req, res, next) => {
-
-
   let orderList;
 
   const {
     supplierName
   } = req.body;
 
-
   try {
-    orderList = await Orders.find(
-      {
-        status: 'supRejected',
-        supplierName: supplierName
-      });
-
-    console.log(orderList);
-
-
+    orderList = await Orders.find({
+      status: 'supRejected',
+      supplierName: supplierName
+    });
   } catch (error) {
     console.log(error);
     return next(new HttpError('Unexpected internal server error occurred, please try again later.', 500));
   }
+
   res.status(200).send(orderList);
-
-
 };
 
-
 const getDeliveryOrdersDManager = async (req, res, next) => {
-
-
   let orderList;
 
   try {
     orderList = await Orders.find({
       status: 'supDelivered'
-
     });
-
-    console.log(orderList);
-
-
   } catch (error) {
     console.log(error);
     return next(new HttpError('Unexpected internal server error occurred, please try again later.', 500));
   }
+
   res.status(200).send(orderList);
-
-
 };
 
 const getDeliveryConfirmedDManager = async (req, res, next) => {
-
-
   let orderList;
 
   try {
     orderList = await Orders.find({
       status: 'deliveryConfirmed'
-
     });
-
-    console.log(orderList);
-
-
   } catch (error) {
     console.log(error);
     return next(new HttpError('Unexpected internal server error occurred, please try again later.', 500));
   }
+
   res.status(200).send(orderList);
-
-
 };
 
-
 const getDeliveryRejectedDManager = async (req, res, next) => {
-
-
   let orderList;
 
   try {
     orderList = await Orders.find({
       status: 'deliveryRejected'
-
     });
-
-    console.log(orderList);
-
-
   } catch (error) {
     console.log(error);
     return next(new HttpError('Unexpected internal server error occurred, please try again later.', 500));
   }
+
   res.status(200).send(orderList);
-
-
 };
 
-
 const getDeliveredOrdersAllSuppliers = async (req, res, next) => {
-
-
   let orderList;
 
   try {
     orderList = await Orders.find({
       status: 'supDelivered'
-
     });
-
-    console.log(orderList);
-
-
   } catch (error) {
     console.log(error);
     return next(new HttpError('Unexpected internal server error occurred, please try again later.', 500));
   }
+
   res.status(200).send(orderList);
-
-
 };
 
 exports.getPendingOrders = getPendingOrders;
